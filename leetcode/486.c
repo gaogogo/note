4eb03c137b0f8d09292625064060376788f2e07d
Gaogogo <cuggsh@163.com>
2018-07-05 13:26:18 +0800

    debug 486

--- leetcode/486.c
+++ leetcode/486.c
@@ -4,56 +4,40 @@
 
 bool PredictTheWinner(int* nums, int numsSize) {
     int i, j;
-    int **f1, **f2, **T;
+    int **f1, **T;
 
     f1 = (int **)malloc(sizeof(int *) * numsSize);
-    f2 = (int **)malloc(sizeof(int *) * numsSize);
     T = (int **)malloc(sizeof(int *) * numsSize);
     for(i=0; i<numsSize; ++i)
     {
         f1[i] = (int *)malloc(sizeof(int) * numsSize);
-        f2[i] = (int *)malloc(sizeof(int) * numsSize);
         T[i] = (int *)malloc(sizeof(int) * numsSize);
     }
 
     for(i=0; i<numsSize; ++i)
     {
-        for(j=numsSize - 1; j>=i; --j)
-        {
-            f1[i][j] = 0;
-            f2[i][j] = 0;
-            if(i == j)
-            {
-                f1[i][j] = f2[i][j] = T[i][j] = nums[i];
-            }
-        }
-    }
-
-    for(i=0; i<numsSize; ++i)
-    {
+        T[i][i] = nums[i];
+        f1[i][i] = nums[i];
         for(j=i+1; j<numsSize; ++j)
         {
             T[i][j] = T[i][j-1] + nums[j];
         }
     }
 
-    for(i=0; i<numsSize; ++i)
+    for(i=1; i < numsSize; ++i)
     {
-        for(j=i+1; j<numsSize; ++j)
+        for(j=0; j+i < numsSize; ++j)
         {
-            f1[i][j] = nums[i]+T[i+1][j]-f2[i+1][j] > nums[j] + T[i][j-1]-f2[i][j-1]
-            ? nums[i]+T[i+1][j]-f2[i+1][j] : nums[j] + T[i][j-1]-f2[i][j-1];
-            f2[i][j] = nums[i]+T[i+1][j]-f1[i+1][j] > nums[j] + T[i][j-1]-f1[i][j-1]
-            ? nums[i]+T[i+1][j]-f1[i+1][j] : nums[j] + T[i][j-1]-f1[i][j-1];
+            f1[j][j+i] = T[j][j+i] - (f1[j][j+i-1] < f1[j+1][j+i] ? f1[j][j+i-1] : f1[j+1][j+i]);
         }
     }
 
-    return f1[0][numsSize-1] > f2[0][numsSize-1];
+    return f1[0][numsSize-1] >= T[0][numsSize-1] - f1[0][numsSize -1];
 }
 
 int main()
 {
-    int a[4] = {1,5,2};
-    bool result = PredictTheWinner(a,3);
+    int a[4] = {1, 5, 233, 7};
+    bool result = PredictTheWinner(a,4);
     printf("%d\n", result);
 }
