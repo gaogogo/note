8ad59639c3a7fb8b652126ecd322e0f7ff70139c
Gaogogo <cuggsh@163.com>
2018-07-08 15:47:10 +0800

    debug 48

--- leetcode/48.c
+++ leetcode/48.c
@@ -6,10 +6,10 @@ void rotate(int** matrix, int matrixRowSize, int *matrixColSizes) {
 
     for(k=0; k < n; k++)
     {
-        for(j=k; j < matrixRowSize-1-k; j++)
+        for(j=0; j < matrixRowSize-1-2k; j++)
         {
-            t = matrix[k][j];
-            matrix[k][j] = matrix[matrixRowSize-1-k-j][k];
+            t = matrix[k][k+j];
+            matrix[k][k+j] = matrix[matrixRowSize-1-k-j][k];
             matrix[matrixRowSize-1-k-j][k] = matrix[matrixRowSize-1-k][matrixRowSize-1-k-j];
             matrix[matrixRowSize-1-k][matrixRowSize-1-k-j] = matrix[k+j][matrixRowSize-1-k];
             matrix[k+j][matrixRowSize-1-k] = t;
